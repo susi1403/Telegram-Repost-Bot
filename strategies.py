6b6ef99122a3c867c17d5e2fce9c91f82574df6f
susi1403 <92388675+susi1403@users.noreply.github.com>
2021-10-19 00:21:38 +0200

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -69,8 +69,6 @@ class _CallOutAllIndividualRepostsStrategy(RepostCalloutStrategy):
                 prev_msg = msg
                 bot.send_message(cid, _format_response_with_name(msg, name), reply_to_message_id=repost_msg)
                 break
-            bot.send_chat_action(cid, ChatAction.TYPING)
-            message.reply_text(_format_response_with_name(self.strings["final_repost_callout"], name))
 
     def _get_random_intermediary_message(self, prev_msg: str):
         return random.choice([response for response in self.strings["intermediary_callouts"] if response != prev_msg])
@@ -83,24 +81,13 @@ class _CallOutNumberOfRepostsStrategy(RepostCalloutStrategy):
 
     @staticmethod
     def get_required_strings() -> List[str]:
-        return ["first_repost_callout", "single_callout_one_repost_options", "single_callout_x_num_reposts_options"]
+        return ["single_callout_one_repost_options", "single_callout_x_num_reposts_options"]
 
     def callout(self, update: Update, context: CallbackContext, hash_to_message_id_dict: Dict[str, List[int]]):
         super().callout(update, context, hash_to_message_id_dict)
         context.bot.send_chat_action(update.effective_chat.id, ChatAction.TYPING)
         num_reposts = sum(len(message_ids) - 1 for message_ids in hash_to_message_id_dict.values())
         name = _get_name_to_use(update)
-        for message_ids in hash_to_message_id_dict.values():
-            prev_msg = ""
-            for i, repost_msg in enumerate(message_ids[:-1]):
-                bot.send_chat_action(cid, ChatAction.TYPING)
-                msg = self._get_random_intermediary_message(prev_msg)
-                if i == 0:
-                    msg = self.strings["first_repost_callout"]
-                    break
-                prev_msg = msg
-                bot.send_message(cid, _format_response_with_name(msg, name), reply_to_message_id=repost_msg)
-            bot.send_chat_action(cid, ChatAction.TYPING)
         key = "single_callout_one_repost_options" if num_reposts == 1 else "single_callout_x_num_reposts_options"
         response_with_num_and_name = _format_response_with_name(random.choice(self.strings[key]), name, num=num_reposts)
         update.message.reply_text(response_with_num_and_name, quote=True)
